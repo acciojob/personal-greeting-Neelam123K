8f8c5b3d6bd1cf52509dab9d83c92c77ce6e3cfa
Neelam123K <153060910+Neelam123K@users.noreply.github.com>
2025-01-06 22:59:45 +0530

    enter your name

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,25 @@
+import React, { useState } from 'react';
 
-import React from "react";
-import './../styles/App.css';
+function GreetingApp() {
+  const [name, setName] = useState(""); 
 
-const App = () => {
+  const handleInputChange = (event) => {
+    setName(event.target.value); 
+  };
+
   return (
-    <div>
-        {/* Do not remove the main div */}
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>Enter your name:</h1>
+      <input
+        type="text"
+        placeholder="Enter your name"
+        value={name}
+        onChange={handleInputChange}
+        style={{ padding: "10px", width: "200px", borderRadius: "5px" }}
+      />
+      {name && <h2>Hello {name}!</h2>}
     </div>
-  )
+  );
 }
 
-export default App
+export default GreetingApp;
